e291785d56a9c5e2b524a9565f3b86ff2047bdcd
Eniko Gazda <89908056+ekgazda@users.noreply.github.com>
2021-12-19 22:59:58 +0100

    update firestore config to new db

--- db/firestoreConfig.ts
+++ db/firestoreConfig.ts
@@ -1,29 +1,25 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
 
 import {
   DB_APIKEY,
   DB_AUTHDOMAIN,
-  DB_URL,
   DB_PROJECTID,
   DB_BUCKET,
   DB_MESSAGINGSENDERID,
   DB_APPID,
-  DB_MEASUREMENTID,
-} from "@env";
+} from '@env'
 
 const firestoreConfig = {
   apiKey: DB_APIKEY,
   authDomain: DB_AUTHDOMAIN,
-  databaseURL: DB_URL,
   projectId: DB_PROJECTID,
   storageBucket: DB_BUCKET,
   messagingSenderId: DB_MESSAGINGSENDERID,
-  appId: DB_APPID,
-  measurementId: DB_MEASUREMENTID,
-};
+  appId: DB_APPID
+}
 
-export const firebaseApp = initializeApp(firestoreConfig);
-export const db = getFirestore();
-export const storage = getStorage(firebaseApp);
+export const firebaseApp = initializeApp(firestoreConfig)
+export const db = getFirestore()
+export const storage = getStorage(firebaseApp)
--- db/seed.ts
+++ db/seed.ts
@@ -1,41 +1,41 @@
-import { db } from "./firestoreConfig";
-import { doc, setDoc } from "firebase/firestore";
-import usersData from "../data/users.json";
-import eventsData from "../data/events.json";
-import chatsData from "../data/chats.json";
+import { db } from './firestoreConfig'
+import { doc, setDoc } from 'firebase/firestore'
+import usersData from '../data/users.json'
+import eventsData from '../data/events.json'
+import chatsData from '../data/chats.json'
 
 export const seedData = () => {
-  const userIdArray = Object.keys(usersData);
-  const eventIdArray = Object.keys(eventsData);
-  const chatIdArray = Object.keys(chatsData);
+  const userIdArray = Object.keys(usersData)
+  const eventIdArray = Object.keys(eventsData)
+  const chatIdArray = Object.keys(chatsData)
 
-  const usersCollectionString = userIdArray.map((item) => `users/${item}`);
+  const usersCollectionString = userIdArray.map((item) => `users/${item}`)
 
-  const eventsCollectionString = eventIdArray.map((item) => `events/${item}`);
+  const eventsCollectionString = eventIdArray.map((item) => `events/${item}`)
 
-  const chatsCollectionString = chatIdArray.map((item) => `chats/${item}`);
+  const chatsCollectionString = chatIdArray.map((item) => `chats/${item}`)
 
   for (let i = 0; i < userIdArray.length; i++) {
-    const newPost = doc(db, usersCollectionString[i]);
-    const docData = usersData[userIdArray[i]];
-        setDoc(newPost, docData).then(() => {
-      console.log("users test data inputted");
-    });
+    const newPost = doc(db, usersCollectionString[i])
+    const docData = usersData[userIdArray[i]]
+    setDoc(newPost, docData).then(() => {
+      console.log('users test data inputted')
+    })
   }
 
   for (let i = 0; i < eventIdArray.length; i++) {
-    const newPost = doc(db, eventsCollectionString[i]);
-    const docData = eventsData[eventIdArray[i]];
+    const newPost = doc(db, eventsCollectionString[i])
+    const docData = eventsData[eventIdArray[i]]
     setDoc(newPost, docData).then(() => {
-      console.log("events test data inputted");
-    });
+      console.log('events test data inputted')
+    })
   }
 
   for (let i = 0; i < chatIdArray.length; i++) {
-    const newPost = doc(db, chatsCollectionString[i]);
-    const docData = chatsData[chatIdArray[i]];
+    const newPost = doc(db, chatsCollectionString[i])
+    const docData = chatsData[chatIdArray[i]]
     setDoc(newPost, docData).then(() => {
-      console.log("chats test data inputted");
-    });
+      console.log('chats test data inputted')
+    })
   }
-};
+}
